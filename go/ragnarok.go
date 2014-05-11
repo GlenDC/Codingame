34ef9587997f739d858c4fc771bfd89c0fa59221
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-05-11 23:51:20 +0200

    Ragnarok now makes use of multithreading

--- go/ragnarok.go
+++ go/ragnarok.go
@@ -1,42 +1,49 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type position struct {
-    x, y int   
+	x, y int
+}
+
+type result struct {
+	value     int
+	direction string
+}
+
+func GetDirection(a, b string, x, y, v int) <-chan result {
+	ch := make(chan result)
+	go func() {
+		difference := x - y
+		switch {
+		case difference < 0:
+			ch <- result{v - 1, a}
+		case difference > 0:
+			ch <- result{v + 1, b}
+		default:
+			ch <- result{v, ""}
+		}
+		close(ch)
+	}()
+	return ch
 }
 
 func main() {
-    var target, thor position
-    fmt.Scanf("%d %d %d %d\n", &target.x, &target.y, &thor.x, &thor.y)
+	var target, thor position
+	fmt.Scanf("%d %d %d %d\n", &target.x, &target.y, &thor.x, &thor.y)
 
-    for {
-        var direction string
-        var difference position
+	for {
+		channel_b := GetDirection("N", "S", target.y, thor.y, thor.y)
+		channel_a := GetDirection("E", "W", thor.x, target.x, thor.x)
 
-        difference.y = target.y - thor.y
-        difference.x = target.x - thor.x
+		result_b := <-channel_b
+		result_a := <-channel_a
 
-        switch  {
-         case difference.y < 0:
-            direction += "N"
-            thor.y--
-         case difference.y > 0:
-            direction += "S"
-            thor.y++
-        }
+		thor.y = result_b.value
+		thor.x = result_a.value
 
-        switch  {
-         case difference.x > 0:
-            direction += "E"
-            thor.x--
-         case difference.x < 0:
-            direction += "W"
-            thor.x++
-        }
-        
-        fmt.Println(direction)
-    }
+		fmt.Println(result_b.direction + result_a.direction)
+	}
 }
