6db1477a7ddd80f08e5be0461220ed0016024fbd
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-05-17 22:01:17 +0200

    Running all tests at once, and output is now optional

--- go/super_computer.go
+++ go/super_computer.go
@@ -21,6 +21,7 @@ func (p *program) Update(input string) {
 
 		if _, ok := p.Calculations[start]; !ok {
 			p.Calculations[start] = duration
+
 		}
 
 		//for k,v := range p.Calculations {
@@ -32,25 +33,38 @@ func (p *program) GetOutput() string {
 	return fmt.Sprintf("%d\n", len(p.Calculations))
 }
 
+func RunProgram(id int) (string, bool) {
+	input := fmt.Sprintf("../input/super_computer_%d.txt", id)
+	ch, ok := cgreader.RunProgram(input)
+	app := program{}
+	for <-ok {
+		app.Update(<-ch)
+	}
+	output := app.GetOutput()
+	test := fmt.Sprintf("../output/super_computer_%d.txt", id)
+	valid := cgreader.TestProgram(test, output)
+	return output, valid
+}
+
 func main() {
 	flag.Parse()
 	n := flag.NArg()
 	if n > 0 {
-		input := fmt.Sprintf("../input/super_computer_%s.txt", flag.Arg(0))
-		ch, ok := cgreader.RunProgram(input)
-		app := program{}
-		for <-ok {
-			app.Update(<-ch)
+		var count int
+		fmt.Sscanf(flag.Arg(0), "%d", &count)
+		results := make([]bool, count)
+		for i := 0; i < count; i++ {
+			output, ok := RunProgram(i + 1)
+			results[i] = ok
+			if n > 1 {
+				fmt.Printf("%s\n\n", output)
+			}
 		}
-		output := app.GetOutput()
-		fmt.Printf("Output:\n%s\n", output)
-		if n > 1 {
-			test := fmt.Sprintf("../output/super_computer_%s.txt", flag.Arg(0))
-			fmt.Print("Test: ")
-			if cgreader.TestProgram(test, output) {
-				fmt.Print("Program is correct")
+		for i, result := range results {
+			if result {
+				fmt.Printf("Program #%d is correct!\n", i+1)
 			} else {
-				fmt.Print("Program is incorrect")
+				fmt.Printf("Program #%d is incorrect!\n", i+1)
 			}
 		}
 	}
