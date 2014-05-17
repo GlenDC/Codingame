7a276c31bc410baabd7fc6f4a7b95ee8357cc89f
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-05-17 15:30:33 +0200

    Super computer passes test 1

--- go/super_computer.go
+++ go/super_computer.go
@@ -6,20 +6,52 @@ import (
 	"github.com/glendc/cgreader"
 )
 
-type program struct{}
+type program struct {
+	N            int
+	Calculations map[int]int
+}
 
 func (p *program) Update(input string) {
-	fmt.Println(input)
+	if p.N == 0 {
+		fmt.Sscanf(input, "%d\n", &p.N)
+		p.Calculations = make(map[int]int, p.N)
+	} else {
+		var start, duration int
+		fmt.Sscanf(input, "%d %d\n", &start, &duration)
+
+		if _, ok := p.Calculations[start]; !ok {
+			p.Calculations[start] = duration
+		}
+
+		//for k,v := range p.Calculations {
+		//}
+	}
+}
+
+func (p *program) GetOutput() string {
+	return fmt.Sprintf("%d\n", len(p.Calculations))
 }
 
 func main() {
 	flag.Parse()
-	if flag.NArg() == 1 {
+	n := flag.NArg()
+	if n > 0 {
 		input := fmt.Sprintf("../input/super_computer_%s.txt", flag.Arg(0))
 		ch, ok := cgreader.RunProgram(input)
 		app := program{}
 		for <-ok {
 			app.Update(<-ch)
+		}
+		output := app.GetOutput()
+		fmt.Printf("Output:\n%s\n", output)
+		if n > 1 {
+			test := fmt.Sprintf("../output/super_computer_%s.txt", flag.Arg(0))
+			fmt.Print("Test: ")
+			if cgreader.TestProgram(test, output) {
+				fmt.Print("Program is correct")
+			} else {
+				fmt.Print("Program is incorrect")
+			}
 		}
 	}
 }
