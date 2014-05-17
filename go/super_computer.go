a47a99f5624ae13f5067fa6d96b0cf2843e37d4a
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-05-17 14:35:52 +0200

    Using cgreader we can now get the input of a test

--- go/super_computer.go
+++ go/super_computer.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "flag"
-    "github.com/glendc/cgreader"
+	"flag"
+	"fmt"
+	"github.com/glendc/cgreader"
 )
 
 func main() {
-    flag.Parse()
-    if flag.NArg() == 1 {
-        input := fmt.Sprintf("../input/super_computer_%s.txt", flag.Arg(0))
-        cgreader.RunProgram(input)
-    }
+	flag.Parse()
+	if flag.NArg() == 1 {
+		input := fmt.Sprintf("../input/super_computer_%s.txt", flag.Arg(0))
+		ch, ok := cgreader.RunProgram(input)
+		for <-ok {
+			fmt.Println(<-ch)
+		}
+	}
 }
