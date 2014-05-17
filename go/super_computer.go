f74086d483f8451af5f3f8065117079ac3bd8e38
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-05-17 14:39:37 +0200

    seperating program logic from io

--- go/super_computer.go
+++ go/super_computer.go
@@ -6,13 +6,20 @@ import (
 	"github.com/glendc/cgreader"
 )
 
+type program struct{}
+
+func (p *program) Update(input string) {
+	fmt.Println(input)
+}
+
 func main() {
 	flag.Parse()
 	if flag.NArg() == 1 {
 		input := fmt.Sprintf("../input/super_computer_%s.txt", flag.Arg(0))
 		ch, ok := cgreader.RunProgram(input)
+		app := program{}
 		for <-ok {
-			fmt.Println(<-ch)
+			app.Update(<-ch)
 		}
 	}
 }
