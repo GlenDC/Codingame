b2e6bf6383fdcffe0da1112d80f85ec848fb4ab7
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-05-19 13:59:34 +0200

    Cleaned up ascii_art file

--- go/ascii_art.go
+++ go/ascii_art.go
@@ -2,55 +2,48 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"github.com/glendc/cgreader"
+	"strings"
 )
 
 func main() {
-	progam_id := 1
+	cgreader.RunAndValidateProgramManual(
+		"../input/ascii_1.txt",
+		"../output/ascii_%d.txt",
+		true,
+		func(ch <-chan string) string {
+			var width, height int
+			var text string
 
-	input := fmt.Sprintf("../input/ascii_%d.txt", progam_id)
-	ch := cgreader.GetManualInput(input)
+			fmt.Sscanln(<-ch, &width)
+			fmt.Sscanln(<-ch, &height)
+			fmt.Sscanln(<-ch, &text)
 
-	var width, height int
-	var text string
+			text = strings.ToUpper(text)
 
-	fmt.Sscanln(<-ch, &width)
-	fmt.Sscanln(<-ch, &height)
-	fmt.Sscanln(<-ch, &text)
+			ascii := make([]string, height)
+			for i := 0; i < height; i++ {
+				ascii[i] = <-ch
+			}
 
-	text = strings.ToUpper(text)
+			output := make([]string, height)
+			for _, char := range text {
+				character := int(char) - 65
+				if character < 0 || character > 26 {
+					character = 26
+				}
+				for i := range output {
+					position := character * width
+					output[i] += ascii[i][position : position+width]
+				}
+			}
 
-	ascii := make([]string, height)
-	for i := 0; i < height; i++ {
-		ascii[i] = <-ch
-	}
+			var program_output string
 
-	output := make([]string, height)
-	for _, char := range text {
-		character := int(char) - 65
-		if character < 0 || character > 26 {
-			character = 26
-		}
-		for i := range output {
-			position := character * width
-			output[i] += ascii[i][position:position+width]
-		}
-	}
+			for _, line := range output {
+				program_output += line + "\n"
+			}
 
-	var program_output string
-
-	for _, line := range output {
-		fmt.Printf("%s\n", line)
-		program_output += line + "\n"
-	}
-
-	test := fmt.Sprintf("../output/ascii_%d.txt", progam_id)
-	result := cgreader.TestOutput(test, program_output)
-
-	if result {
-		fmt.Printf("Program #%d is correct!\n", progam_id)
-	} else {
-		fmt.Printf("Program #%d is incorrect!\n", progam_id)
-	}
+			return program_output
+		})
 }
