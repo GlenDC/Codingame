eae26c3f399c1afada9032a9c6ccd37dbe618a11
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-05-19 13:37:09 +0200

    Transformd ascii_art to be used with cgreader

--- go/ascii_art.go
+++ go/ascii_art.go
@@ -1,28 +1,32 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"strings"
+	"github.com/glendc/cgreader"
 )
 
 func main() {
+	progam_id := 1
+
+	input := fmt.Sprintf("../input/ascii_%d.txt", progam_id)
+	ch := cgreader.GetManualInput(input)
+
 	var width, height int
 	var text string
-	fmt.Scanf("%d\n%d\n%s\n", &width, &height, &text)
+
+	fmt.Sscanln(<-ch, &width)
+	fmt.Sscanln(<-ch, &height)
+	fmt.Sscanln(<-ch, &text)
 
 	text = strings.ToUpper(text)
 
-	reader := bufio.NewReader(os.Stdin)
-
-	ascii := make([]string, height, height)
+	ascii := make([]string, height)
 	for i := 0; i < height; i++ {
-		text, _ := reader.ReadString('\n')
-		ascii[i] = text
+		ascii[i] = <-ch
 	}
 
-	output := make([]string, height, height)
+	output := make([]string, height)
 	for _, char := range text {
 		character := int(char) - 65
 		if character < 0 || character > 26 {
@@ -30,11 +34,23 @@ func main() {
 		}
 		for i := range output {
 			position := character * width
-			output[i] += ascii[i][position : position+width]
+			output[i] += ascii[i][position:position+width]
 		}
 	}
 
+	var program_output string
+
 	for _, line := range output {
-		fmt.Println(line)
+		fmt.Printf("%s\n", line)
+		program_output += line + "\n"
+	}
+
+	test := fmt.Sprintf("../output/ascii_%d.txt", progam_id)
+	result := cgreader.TestOutput(test, program_output)
+
+	if result {
+		fmt.Printf("Program #%d is correct!\n", progam_id)
+	} else {
+		fmt.Printf("Program #%d is incorrect!\n", progam_id)
 	}
 }
