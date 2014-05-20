52a121b9f12c77cb14f1da140f6e2338a4889266
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-05-21 01:38:03 +0200

    Applied flow program style to super computer

--- go/super_computer.go
+++ go/super_computer.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/glendc/cgreader"
 )
 
-type program struct {
+type Program struct {
 	N            int
 	Calculations map[int]int
 }
 
-func (p *program) Update(input string) {
+func (p *Program) Update(input string) {
 	if p.N == 0 {
 		fmt.Sscanf(input, "%d\n", &p.N)
 		p.Calculations = make(map[int]int, p.N)
@@ -35,43 +34,14 @@ func (p *program) Update(input string) {
 	}
 }
 
-func (p *program) GetOutput() string {
+func (p *Program) GetOutput() string {
 	return fmt.Sprintf("%d\n", len(p.Calculations))
 }
 
-func RunProgram(id int) (string, bool) {
-	input := fmt.Sprintf("../input/super_computer_%d.txt", id)
-	ch, ok := cgreader.GetInput(input)
-	app := program{}
-	for <-ok {
-		app.Update(<-ch)
-	}
-	output := app.GetOutput()
-	test := fmt.Sprintf("../output/super_computer_%d.txt", id)
-	valid := cgreader.TestOutput(test, output)
-	return output, valid
-}
-
 func main() {
-	flag.Parse()
-	n := flag.NArg()
-	if n > 0 {
-		var count int
-		fmt.Sscanf(flag.Arg(0), "%d", &count)
-		results := make([]bool, count)
-		for i := 0; i < count; i++ {
-			output, ok := RunProgram(i + 1)
-			results[i] = ok
-			if n > 1 {
-				fmt.Printf("%s\n\n", output)
-			}
-		}
-		for i, result := range results {
-			if result {
-				fmt.Printf("Program #%d is correct!\n", i+1)
-			} else {
-				fmt.Printf("Program #%d is incorrect!\n", i+1)
-			}
-		}
-	}
+	cgreader.RunAndValidateFlowProgram(
+		"../input/super_computer_1.txt",
+		"../output/super_computer_1.txt",
+		true,
+		&Program{})
 }
