7977203c7b9e4404252ef0069ea7eb82ba213945
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-06-02 11:57:09 +0200

    ragnarok challenge is now clean and similar to the online version

--- solutions/go/ragnarok.go
+++ solutions/go/ragnarok.go
@@ -3,27 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/glendc/cgreader"
-	"strings"
 )
-
-type Vector struct {
-	x, y int
-	icon string
-}
-
-func (v Vector) GetMapCoordinates() string {
-	return fmt.Sprintf("%d;%d", v.x, v.y)
-}
-
-func (v Vector) GetMapIcon() string {
-	return v.icon
-}
-
-type Ragnarok struct {
-	thor, target, dimensions Vector
-	energy                   int
-	trail                    []Vector
-}
 
 func GetDirection(x, y int) <-chan int {
 	ch := make(chan int)
@@ -44,46 +24,23 @@ func GetDirection(x, y int) <-chan int {
 
 func GetDirectionLetter(a, b string, v int) string {
 	switch v {
-	default:
-		return ""
 	case -1:
 		return a
 	case 1:
 		return b
+	default:
+		return ""
 	}
 }
 
 var TX, TY, PX, PY, E int
 
-func (ragnarok *Ragnarok) ParseInitialData(ch <-chan string) {
-	fmt.Sscanf(
-		<-ch,
-		"%d %d %d %d %d %d %d \n",
-		&ragnarok.dimensions.x,
-		&ragnarok.dimensions.y,
-		&ragnarok.thor.x,
-		&ragnarok.thor.y,
-		&ragnarok.target.x,
-		&ragnarok.target.y,
-		&ragnarok.energy)
-
-	TX, TY = ragnarok.thor.x, ragnarok.thor.y
-	PX, PY = ragnarok.target.x, ragnarok.target.y
-	E = ragnarok.energy
-
-	ragnarok.thor.icon, ragnarok.target.icon = "H", "T"
+func Initialize(input string) {
+	fmt.Sscanf(input, "%d %d %d %d", &PX, &PY, &TX, &TY)
 }
 
-func (ragnarok *Ragnarok) GetInput() (ch chan string) {
-	ch = make(chan string)
-	go func() {
-		ch <- fmt.Sprintf("%d", ragnarok.energy)
-	}()
-	return
-}
-
-func (ragnarok *Ragnarok) Update(ch <-chan string) string {
-	fmt.Sscanf(<-ch, "%d", &E)
+func Update(input string) string {
+	fmt.Sscanf(input, "%d", &E)
 
 	chx := GetDirection(PX, TX)
 	chy := GetDirection(PY, TY)
@@ -94,68 +51,9 @@ func (ragnarok *Ragnarok) Update(ch <-chan string) string {
 
 	TX, TY = TX+dx, TY+dy
 
-	return y+x
-}
-
-func (ragnarok *Ragnarok) SetOutput(output string) string {
-	ragnarok.trail = append(ragnarok.trail, Vector{ragnarok.thor.x, ragnarok.thor.y, "+"})
-
-	if strings.Contains(output, "N") {
-		ragnarok.thor.y -= 1
-	} else if strings.Contains(output, "S") {
-		ragnarok.thor.y += 1
-	}
-
-	if strings.Contains(output, "E") {
-		ragnarok.thor.x += 1
-	} else if strings.Contains(output, "W") {
-		ragnarok.thor.x -= 1
-	}
-
-	ragnarok.energy -= 1
-
-	trail := append(ragnarok.trail, ragnarok.thor, ragnarok.target)
-
-	map_info := make([]cgreader.MapObject, len(trail))
-	for i, v := range trail {
-		map_info[i] = cgreader.MapObject(v)
-	}
-
-	cgreader.DrawMap(
-		ragnarok.dimensions.x,
-		ragnarok.dimensions.y,
-		".",
-		map_info...)
-
-	return fmt.Sprintf(
-		"Target = (%d,%d)\nThor = (%d,%d)\nEnergy = %d",
-		ragnarok.target.x,
-		ragnarok.target.y,
-		ragnarok.thor.x,
-		ragnarok.thor.y,
-		ragnarok.energy)
-}
-
-func (ragnarok *Ragnarok) LoseConditionCheck() bool {
-	if ragnarok.energy <= 0 {
-		return true
-	}
-
-	x, y := ragnarok.thor.x, ragnarok.thor.y
-	dx, dy := ragnarok.dimensions.x, ragnarok.dimensions.y
-
-	if x < 0 || x >= dx || y < 0 || y >= dy {
-		return true
-	}
-
-	return false
-}
-
-func (ragnarok *Ragnarok) WinConditionCheck() bool {
-	return ragnarok.target.x == ragnarok.thor.x &&
-		ragnarok.target.y == ragnarok.thor.y
+	return y + x
 }
 
 func main() {
-	cgreader.RunTargetProgram("../../input/ragnarok_3.txt", true, &Ragnarok{})
+	cgreader.RunRagnarok("../../input/ragnarok_1.txt", true, Initialize, Update)
 }
