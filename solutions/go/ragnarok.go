6555a0b483b8b683159e30011b1a398956e489cd
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-06-05 19:55:01 +0200

    delaying it

--- solutions/go/ragnarok.go
+++ solutions/go/ragnarok.go
@@ -55,5 +55,6 @@ func Update(input <-chan string, output chan string) {
 }
 
 func main() {
+	cgreader.SetFrameRate(30)
 	cgreader.RunRagnarok("../../input/ragnarok_1.txt", true, Initialize, Update)
 }
