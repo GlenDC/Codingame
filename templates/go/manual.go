632a01aefa760603783345b5b44a8988c3aa8dbf
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-06-08 00:44:01 +0200

    adding template for manual programs

---
+++ templates/go/manual.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "github.com/glendc/cgreader"
+)
+
+func main() {
+    cgreader.RunAndValidateManualProgram(
+        "../../input/input.txt",
+        "../../output/output.txt",
+        true,
+        func(input <-chan string, output chan string) {
+            // program logic
+        })
+}
