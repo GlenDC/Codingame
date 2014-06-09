7c81f9c73f652da0ad1bedec67fc32b4de288822
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-06-09 13:45:29 +0200

    stock exchange go solution improved

--- solutions/go/stock_exchange_losses.go
+++ solutions/go/stock_exchange_losses.go
@@ -1,47 +1,52 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "github.com/glendc/cgreader"
+	"fmt"
+	"github.com/glendc/cgreader"
+	"strings"
 )
 
 func main() {
-    cgreader.RunAndValidateManualPrograms(
-        []string{
-            "../../input/stock_exchange_losses_1.txt",
-            "../../input/stock_exchange_losses_2.txt",
-            "../../input/stock_exchange_losses_3.txt",
-            "../../input/stock_exchange_losses_4.txt",
-            "../../input/stock_exchange_losses_5.txt"},
-        []string{
-            "../../output/stock_exchange_losses_1.txt",
-            "../../output/stock_exchange_losses_2.txt",
-            "../../output/stock_exchange_losses_3.txt",
-            "../../output/stock_exchange_losses_4.txt",
-            "../../output/stock_exchange_losses_5.txt"},
-        true,
-        func(input <-chan string, output chan string) {
-            var n int
-            fmt.Sscanf(<-input, "%d", &n)
+	cgreader.RunAndValidateManualPrograms(
+		[]string{
+			"../../input/stock_exchange_losses_1.txt",
+			"../../input/stock_exchange_losses_2.txt",
+			"../../input/stock_exchange_losses_3.txt",
+			"../../input/stock_exchange_losses_4.txt",
+			"../../input/stock_exchange_losses_5.txt"},
+		[]string{
+			"../../output/stock_exchange_losses_1.txt",
+			"../../output/stock_exchange_losses_2.txt",
+			"../../output/stock_exchange_losses_3.txt",
+			"../../output/stock_exchange_losses_4.txt",
+			"../../output/stock_exchange_losses_5.txt"},
+		true,
+		func(input <-chan string, output chan string) {
+			var ml, cm, v int64 = 0, 0, 0
+			var n, i int
+			fmt.Sscanf(<-input, "%d", &n)
 
-            s := strings.Split(<-input, " ")
-            stocks := make([]int, n)
-            for i := range s {
-                fmt.Sscanf(s[i], "%d", &stocks[i])
-            }
+			if n == 0 {
+				output <- fmt.Sprint("0")
+				return
+			}
 
-            var difference int
-            for i := 0; i < len(stocks) - 1; i++ {
-                for u := i+1; u < len(stocks); u++ {
-                    if i != u {
-                        if d := stocks[u] - stocks[i]; d < difference {
-                            difference = d
-                        }
-                    }
-                }
-            }
+			stocks := strings.Split(<-input, " ")
+			fmt.Sscanf(stocks[i], "%d", &v)
+			cm = v
 
-            output <- fmt.Sprintf("%d", difference)
-        })
+			for i = 1; i < n; i++ {
+				fmt.Sscanf(stocks[i], "%d", &v)
+
+				if d := v - cm; d < ml {
+					ml = d
+				}
+
+				if v > cm {
+					cm = v
+				}
+			}
+
+			output <- fmt.Sprintf("%d", ml)
+		})
 }
