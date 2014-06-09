5d29319ca291b2df6a361e8c4fd822085ae898aa
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-06-10 01:06:33 +0200

    adding auto template for manual programs

--- templates/go/run_validate_manual_programs_auto.go
+++ templates/go/run_validate_manual_programs_auto.go
@@ -7,12 +7,8 @@ import (
 
 func main() {
 	cgreader.RunAndValidateManualPrograms(
-		[]string{
-			"../../input/input_1.txt",
-			"../../input/input_2.txt"},
-		[]string{
-			"../../output/output_1.txt",
-			"../../output/output_2.txt"},
+		cgreader.GetFileList("../../input/input_%d.txt", 2),
+		cgreader.GetFileList("../../output/output_%d.txt", 2),
 		true,
 		func(input <-chan string, output chan string) {
 			// program logic
