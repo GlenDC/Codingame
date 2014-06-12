14ee835a32717b5fa8e5c0e7ca896922db4c83e5
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-06-12 21:33:35 +0200

    adding solution for telephone number

---
+++ solutions/go/telephone_number.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/glendc/cgreader"
+)
+
+type Node struct {
+	nodes map[int]Node
+}
+
+func CreateNode() Node {
+	return Node{make(map[int]Node)}
+}
+
+func main() {
+	cgreader.RunAndValidateManualPrograms(
+		cgreader.GetFileList("../../input/telephone_number_%d.txt", 5),
+		cgreader.GetFileList("../../output/telephone_number_%d.txt", 5),
+		true,
+		func(input <-chan string, output chan string) {
+			var n int
+			fmt.Sscanf(<-input, "%d", &n)
+
+			counter, database := 0, CreateNode()
+			for i := 0; i < n; i++ {
+				var tel string
+				fmt.Sscanf(<-input, "%s", &tel)
+
+				it := database
+				for u := range tel {
+					var number int
+					fmt.Sscanf(string(tel[u]), "%d", &number)
+
+					if _, ok := it.nodes[number]; !ok {
+						it.nodes[number] = CreateNode()
+						counter++
+					}
+
+					it = it.nodes[number]
+				}
+			}
+
+			output <- fmt.Sprintf("%d", counter)
+		})
+}
